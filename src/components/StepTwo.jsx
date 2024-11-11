bd076971451ab09b53ab46d2fe855335bcb2e547
Happy Phiri <68534793+happy-phiri@users.noreply.github.com>
2024-11-11 11:45:04 +0200

    edit styling for step two

--- src/components/StepTwo.jsx
+++ src/components/StepTwo.jsx
@@ -80,7 +80,7 @@ const StepTwo = () => {
         });
 
   return (
-    <section className="font-ubuntu max-lg:absolute top-[18%] left-0 right-0 w-[90%] mx-auto lg:bg-white lg:w-full lg:h-full lg:py-7 lg:rounded-tr-lg lg:rounded-br-lg">
+    <section className="font-ubuntu max-lg:absolute top-[20%] left-0 right-0 w-[90%] mx-auto lg:bg-white lg:w-full lg:h-full lg:py-7 lg:rounded-tr-lg lg:rounded-br-lg">
       <div className="max-lg:bg-white max-lg:pb-7 rounded-lg">
         <div className="lg:w-[80%] max-lg:px-4 max-lg:py-8 max-lg:rounded-lg mx-auto bg-white ">
           <h2 className="text-2xl lg:text-4xl text-marine-blue tracking-wide">
